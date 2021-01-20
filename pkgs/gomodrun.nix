f6db9388b41bfd92d8b364682fca4212c185e44b
Dustin Blackman <dev@dustinblackman.com>
2021-01-19 20:32:13 -0500

    fix tests

--- pkgs/gomodrun.nix
+++ pkgs/gomodrun.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation {
   name = "gomodrun";
-  src = builtins.fetchurl {
+  src = pkgs.fetchurl {
     url = "https://github.com/dustinblackman/gomodrun/releases/download/v0.4.1/gomodrun_0.4.1_linux_amd64.tar.gz";
     sha256 = "16pq9z2fl7qnz6z56bv57snin5iq1lg3nqclzi3bp3p1sqfkcbyi";
   };
