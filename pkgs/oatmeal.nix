b2d43acd80bb3c894fd9489f8a6c4cc6198d8a22
Dustin Blackman <dev@dustinblackman.com>
2023-11-10 12:14:25 -0500

    oatmeal 0.2.2

--- pkgs/oatmeal.nix
+++ pkgs/oatmeal.nix
@@ -4,8 +4,8 @@
 pkgs.stdenv.mkDerivation {
   name = "oatmeal";
   src = pkgs.fetchurl {
-    url = "https://github.com/dustinblackman/oatmeal/releases/download/v0.2.1/oatmeal_0.2.1_linux_amd64.tar.gz";
-    sha256 = "18plp9dh805c4ms2bsyxpq9y683axp7iidqhbbm8swkgzkmp9dbl";
+    url = "https://github.com/dustinblackman/oatmeal/releases/download/v0.2.2/oatmeal_0.2.2_linux_amd64.tar.gz";
+    sha256 = "1r8h3bm5z67kyrjlrzrljs3vm282mx3vcsn9ss17m6v3zm8k69pk";
   };
   phases = [ "installPhase" ];
   installPhase = ''
